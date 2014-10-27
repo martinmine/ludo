11f72efb6e6e496e0666ad7ea48f0c3cd3cab587
martinmine <martin_mine@hotmail.com>
2014-10-27 16:32:41 +0100

    Initial commit

---
+++ backend/src/main/java/no/hig/imt3281/ludo/backend/Main.java
@@ -0,0 +1,10 @@
+package no.hig.imt3281.ludo.backend;
+
+/**
+ * Created by Martin on 27.10.2014.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
