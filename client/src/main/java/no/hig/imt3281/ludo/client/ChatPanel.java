b1df1bc4b98f71136e73d4e5de75797631ee11de
JoakimJoreng <jjoereng@gmail.com>
2014-11-03 15:31:44 +0100

    added chat panel class

---
+++ client/src/main/java/no/hig/imt3281/ludo/client/ChatPanel.java
@@ -0,0 +1,12 @@
+package no.hig.imt3281.ludo.client;
+
+import javax.swing.*;
+
+/**
+ * Created by Joakim on 03.11.2014.
+ */
+public class ChatPanel extends JPanel {
+    public ChatPanel() {
+        
+    }
+}
