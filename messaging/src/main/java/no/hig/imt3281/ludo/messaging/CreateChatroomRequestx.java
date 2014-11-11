e3ee0dd131f957bff940ecf88c651fcf82f4757f
JoakimJoreng <jjoereng@gmail.com>
2014-11-11 15:48:23 +0100

    fix

--- messaging/src/main/java/no/hig/imt3281/ludo/messaging/CreateChatroomRequestx.java
+++ messaging/src/main/java/no/hig/imt3281/ludo/messaging/CreateChatroomRequestx.java
@@ -3,10 +3,10 @@ package no.hig.imt3281.ludo.messaging;
 /**
  * Created by Joakim on 11.11.2014.
  */
-public class CreateChatRoomRequest extends Message {
+public class CreateChatroomRequestx extends Message {
     private String chatroomName;
 
-    public CreateChatRoomRequest(String chatroomName) {
+    public CreateChatroomRequestx(String chatroomName) {
         this.chatroomName = chatroomName;
     }
 
