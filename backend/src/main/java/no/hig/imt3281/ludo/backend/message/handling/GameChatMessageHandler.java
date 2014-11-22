bc25c27d4b739fc592282de6cd69e1c5969605c9
martinmine <martin_mine@hotmail.com>
2014-11-22 14:46:47 +0100

    Completed game chat

--- backend/src/main/java/no/hig/imt3281/ludo/backend/message/handling/GameChatMessageHandler.java
+++ backend/src/main/java/no/hig/imt3281/ludo/backend/message/handling/GameChatMessageHandler.java
@@ -7,10 +7,13 @@ import no.hig.imt3281.ludo.messaging.GameChatMessage;
 import no.hig.imt3281.ludo.messaging.handling.CommunicationContext;
 import no.hig.imt3281.ludo.messaging.handling.MessageHandler;
 
+import java.util.logging.Logger;
+
 /**
  * Created by Martin on 11.11.2014.
  */
 public class GameChatMessageHandler implements MessageHandler {
+    private static final Logger LOGGER = Logger.getLogger(GameChatMessageHandler.class.getSimpleName());
     public void handle(GameChatMessage request, CommunicationContext context) {
         User user = ServerEnvironment.getUserManager().getUser(context.getReferenceToken());
         GameChat chat = ServerEnvironment.getChatManager().getGameChat(user.getCurrentGameId());
