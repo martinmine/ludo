f19098c83e2c6e61858a1792be971eba8def1e8d
martinmine <martin_mine@hotmail.com>
2014-11-27 23:53:29 +0100

    Fixed sonar issues

--- messaging/src/main/java/no/hig/imt3281/ludo/messaging/TurnMessage.java
+++ messaging/src/main/java/no/hig/imt3281/ludo/messaging/TurnMessage.java
@@ -4,8 +4,5 @@ package no.hig.imt3281.ludo.messaging;
  * Message being sent to the client during a game indicating that the user can roll the dice
  */
 public class TurnMessage extends Message {
-
-    public int RESET_DICE_VALUE = 0;
-
-
+    public static final int RESET_DICE_VALUE = 0;
 }
--- messaging/src/test/java/no/hig/imt3281/ludo/messaging/handling/MessageHandlerFactoryTest.java
+++ messaging/src/test/java/no/hig/imt3281/ludo/messaging/handling/MessageHandlerFactoryTest.java
@@ -3,6 +3,10 @@ package no.hig.imt3281.ludo.messaging.handling;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import static org.junit.Assert.*;
 
 /**
@@ -12,6 +16,7 @@ public class MessageHandlerFactoryTest {
     private static final int NUMBER = 20;
     private static final String WORD = "Test";
     private static final boolean CONDITION = true;
+    private static final Logger LOGGER = Logger.getLogger(MessageHandlerFactoryTest.class.getSimpleName());
 
     private MessageHandlerFactory factory;
     private TestMessageHandler testHandler;
@@ -24,13 +29,18 @@ public class MessageHandlerFactoryTest {
     }
 
     @Test
-    public void testInvokeMessage() throws Exception {
+    public void testInvokeMessage() {
         TestMessage message = new TestMessage();
         message.setIntValue(NUMBER);
         message.setStringValue(WORD);
         message.setBoolValue(CONDITION);
 
-        factory.invokeMessage(message, null);
+        try {
+            factory.invokeMessage(message, null);
+        } catch (MissingMessageHandlerException | InvalidMessageHandlerException | InvocationTargetException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            assertTrue(false);
+        }
 
         assertEquals(message.getIntValue(), testHandler.getIntValue());
         assertEquals(message.getStringValue(), testHandler.getStringValue());
